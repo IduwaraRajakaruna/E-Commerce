6fa81e4c60a876379eeb75510be46b6f24c40d23
IduwaraRajakaruna <iduwararajakaruna5555@gmail.com>
2025-09-21 19:53:29 +0530

    working on backend

---
+++ Backend/util/db.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const dbConnection = async() => {
+    const DB = process.env.DB;
+    console.log("Connecting to database:", DB);
+    try{
+         await mongoose.connect(DB).then(() =>{
+            console.log("Database is connected successfully");
+         })
+    }catch(error){
+        console.error("Database connection error:", error.message);
+        setTimeout(dbConnection, 5000);
+    }
+}
+
+export default dbConnection;
