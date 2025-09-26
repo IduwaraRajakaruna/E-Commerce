8eed5dc7682dd710242650ac9e69ee80fad10dd8
IduwaraRajakaruna <iduwararajakaruna5555@gmail.com>
2025-09-26 23:03:45 +0530

    Backend Authentication

--- Backend/app.js
+++ Backend/app.js
@@ -1,12 +1,25 @@
 import express from 'express';
 import cors from 'cors';
-const app = express();
+import cookieParser from 'cookie-parser';
+import { errorHandler, notFound } from './middleware/error.middleware.js';
+import authRoute from './routes/auth.route.js';
 
+const app = express();  
+
+//cors
+app.use(cors());
 
 //json body
 app.use(express.json());
 
-//cors
-app.use(cors());
+//cookie parser
+app.use(cookieParser());
+
+//routes
+app.use("/api/v1/auth", authRoute);
+
+//error middleware
+app.use(notFound);
+app.use(errorHandler);
 
 export default app;
