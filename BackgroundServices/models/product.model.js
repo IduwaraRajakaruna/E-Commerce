6527edf7251193d7e6a4e1f388da5e6248ede2f4
IduwaraRajakaruna <iduwararajakaruna5555@gmail.com>
2025-10-07 19:03:27 +0530

    added welcome email background services

---
+++ BackgroundServices/models/product.model.js
@@ -0,0 +1,69 @@
+import mongoose from "mongoose";
+const ProductSchema = mongoose.Schema({
+
+    title:{
+        type:String,
+        require:true,       
+    },
+    desc:{
+        type:String,
+        require:true,
+    },
+    whatinbox:{
+        type:String
+    },
+    img:{
+        type:String,
+        require:true,
+    },
+    video:{
+        type:String
+    },
+    WholesalePrice:{
+        type:Number
+    },
+    wholesaleMinimumQuantity:{
+        type:Number 
+    },
+    categories:{
+        type:Array
+    },
+    title:{
+        type:String,
+        require:true,       
+    },
+    concern:{
+        type:Array     
+    },
+    brand:{
+        type:String     
+    },
+    skintype:{
+        type:Array      
+    },
+    originalPrice:{
+        type:Number  
+    },
+    discountedPrice:{
+        type:Number    
+    },
+    inStock:{
+        type:Boolean,
+        default:true,       
+    },
+    ratings:[
+        {
+            star:{type:String},
+            name:{type:String},
+            comment:{type:String}, 
+            postedBy:{type:String}
+        }
+    ]
+    
+
+});
+
+ProductSchema.index({"$**" : "text"})
+const Product = mongoose.model("Product", ProductSchema);
+
+export default Product;
