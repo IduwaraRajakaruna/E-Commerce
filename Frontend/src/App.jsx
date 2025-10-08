c6f1ad7fd8336ccc60c782b97f8d992b43e65bdb
IduwaraRajakaruna <iduwararajakaruna5555@gmail.com>
2025-10-09 00:01:16 +0530

    add pages

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,10 +1,36 @@
+import Cart from "./pages/Cart";
+import Home from "./pages/Home";
+import {RouterProvider, createBrowserRouter} from "react-router-dom";
+import Myaccount from "./pages/Myaccount";
+import Navbar from "./components/Navbar";
 
 function App() { 
 
+  const Layout = () => {
+    return 
+    <div>
+      <Navbar />
+    </div>;
+  };
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Home />
+    },
+    {
+      path: "/cart",
+      element: <Cart />
+    },
+    {
+      path: "/myaccount",
+      element: <Myaccount />
+    }
+  ]);
   return (
-    <>
-     <h1 className="text-8xl">test 1</h1>
-    </>
+    <div>
+     <RouterProvider router={router} />
+    </div>
   );
 }
 
