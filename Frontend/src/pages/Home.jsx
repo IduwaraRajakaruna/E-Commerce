0857ce5f0e4894a7d8e5c81b79f3cfabf6df111a
IduwaraRajakaruna <iduwararajakaruna5555@gmail.com>
2025-10-10 23:22:08 +0530

    go back last comiit and add a pakege

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import Banner from "../components/Banner"
 import Category from "../components/Category"
+import Products from "../components/Products"
 
 const Home = () => {
   return (
     <div>
       <Banner />
       <Category />
+      <Products />
     </div>
   )
 }
